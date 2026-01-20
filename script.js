2225361ae2d71bd2862e71ac262d2581b1ca2708
Aksheen Rathod <aksheenrathod@Aksheens-Air.attlocal.net>
2026-01-19 21:16:11 -0800

    Backend done Frontend developed

---
+++ script.js
@@ -0,0 +1,95 @@
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+const genQuoteBtn = document.getElementById("gen-quote");
+const authorImage = document.getElementById("author-image");
+
+
+const API_KEY = "1pXAljDOMJ8ozH2p96spbfwdAsT3OQ9vea9AcsiT";
+const API_URL = "https://api.api-ninjas.com/v2/randomquotes";
+
+async function generateQuote() {
+    try{
+        const response = await fetch("https://api.api-ninjas.com/v2/randomquotes", {
+        headers: {
+            "X-Api-Key": API_KEY
+        }
+    });
+    const data = await response.json();
+    quoteText.innerText = `"${data[0].quote}"`;
+    quoteAuthor.innerText = `- ${data[0].author}`;
+    fetchAuthorImage(data[0].author);
+    }
+    catch(error){
+        quoteText.innerText = "An error occurred while fetching the quote.";
+        quoteAuthor.innerText = "";
+        authorImage.src = "";
+        console.error("Error fetching quote:", error);
+    }
+}
+
+
+async function fetchAuthorImage(author) {
+    try {
+        const formattedAuthor = author.replaceAll(" ", "_");
+        const response = await fetch(`https://en.wikipedia.org/api/rest_v1/page/summary/${formattedAuthor}`);
+        const data = await response.json();
+        if (data.thumbnail && data.thumbnail.source) {
+            authorImage.src = data.thumbnail.source;
+        } else {
+            authorImage.src = "unknown-author.png";
+        }
+    } catch (error) {
+        console.error("Error fetching author image:", error);
+    }
+}
+genQuoteBtn.addEventListener("click", generateQuote);
+
+generateQuote();
+
+// Star animation
+
+function createStar() {
+    const star = document.createElement("div");
+    star.classList.add("star");
+    star.style.left = Math.random() * 100 + "%";
+    star.style.top = Math.random() * 100 + "%";
+    const starsContainer = document.getElementById("stars");
+    starsContainer.appendChild(star);
+    star.style.animationDuration = (Math.random() * 3 + 2) + "s";
+    setTimeout(() => {
+        star.remove();
+    }, 5000);
+
+}
+
+setInterval(createStar, 10);
+//shooting star animation
+
+function createShootingStar() {
+    const shootingstar = document.createElement("div");
+    shootingstar.classList.add("shootingstar");
+    shootingstar.style.left = Math.random() * 100 + "%";
+    shootingstar.style.top = Math.random() * 100 + "%";
+    const starsContainer = document.getElementById("stars");
+    starsContainer.appendChild(shootingstar);
+    setTimeout(() => {
+        shootingstar.remove();
+    }, 1000);
+
+}
+
+function spawnShootingStar(){
+    createShootingStar();
+    setTimeout(spawnShootingStar, Math.random() * 3000 + 2000);
+}
+spawnShootingStar();
+
+
+
+
+
+
+
+
+
+
